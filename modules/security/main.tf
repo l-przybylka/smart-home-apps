b666063fcd98210cc387f00a7e39d03b3c3cf5f4
Lukasz <lukaszprzybylka@gmail.com>
2023-12-04 12:03:27 +0000

    feat: add http & https security groups

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -16,3 +16,63 @@ resource "aws_vpc_security_group_ingress_rule" "ssh" {
   ip_protocol = "tcp"
   to_port     = 22
 }
+
+resource "aws_security_group" "http" {
+  name        = "allow_http"
+  description = "Allows HTTP connections"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "sg-http"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "http" {
+  security_group_id = aws_security_group.http.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  cidr_ipv6   = "::/0"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "http" {
+  security_group_id = aws_security_group.http.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  cidr_ipv6   = "::/0"
+  from_port   = 3000
+  ip_protocol = "tcp"
+  to_port     = 3000
+}
+
+resource "aws_security_group" "https" {
+  name        = "allow_https"
+  description = "Allows HTTPS connections"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "sg-https"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "https" {
+  security_group_id = aws_security_group.https.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  cidr_ipv6   = "::/0"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "https" {
+  security_group_id = aws_security_group.https.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  cidr_ipv6   = "::/0"
+  from_port   = 3000
+  ip_protocol = "tcp"
+  to_port     = 3000
+}
