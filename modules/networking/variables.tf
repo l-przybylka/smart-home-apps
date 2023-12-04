8110676225a0be9e02bfa9bfd57dccf051832a6e
Lukasz <lukaszprzybylka@gmail.com>
2023-12-04 10:37:13 +0000

    feat: create vpc

---
+++ modules/networking/variables.tf
@@ -0,0 +1,24 @@
+variable "vpc_name" {
+  type        = string
+  description = "Name of the VPC"
+}
+
+variable "cidr" {
+  type        = string
+  description = "CIDR block for the VPC"
+}
+
+variable "public_cidr" {
+  type        = list(string)
+  description = "List of public CIDR blocks"
+}
+
+variable "private_cidr" {
+  type        = list(string)
+  description = "List of private CIDR blocks"
+}
+
+variable "azs" {
+  type        = list(string)
+  description = "List of availability zones for the VPC"
+}
