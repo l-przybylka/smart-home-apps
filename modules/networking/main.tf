63a3cf2e69dd1b4610d65ac270c097ca995c38ce
Lukasz <lukaszprzybylka@gmail.com>
2023-12-04 10:54:24 +0000

    feat: add subnets

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -8,3 +8,19 @@ resource "aws_vpc" "main" {
     Name = var.vpc_name
   }
 }
+
+resource "aws_subnet" "public" {
+  count                   = length(var.public_cidr)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_cidr[count.index]
+  availability_zone       = var.azs[count.index]
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private" {
+  count                   = length(var.private_cidr)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.private_cidr[count.index]
+  availability_zone       = var.azs[count.index]
+  map_public_ip_on_launch = false
+}
