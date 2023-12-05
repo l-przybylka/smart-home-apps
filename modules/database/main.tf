693a371e4f0e04e965ddf6e3e4b6fe04e177c991
Lukasz <lukaszprzybylka@gmail.com>
2023-12-05 09:58:34 +0000

    update: dynamo db table resource

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -9,26 +9,6 @@ resource "aws_dynamodb_table" "lights" {
     type = "N"
   }
 
-  attribute {
-    name = "Location"
-    type = "S"
-  }
-
-  attribute {
-    name = "Status"
-    type = "B"
-  }
-
-  global_secondary_index {
-    name               = "LocationIndex"
-    hash_key           = "Location"
-    range_key          = "Status"
-    write_capacity     = 10
-    read_capacity      = 10
-    projection_type    = "INCLUDE"
-    non_key_attributes = ["id"]
-  }
-
   tags = {
     Name = "dynamo-db-lights"
   }
